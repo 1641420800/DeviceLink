5f9c8e4f0f05466cf0f1511658a665b7f888a808
YxgLcw <1641420800@qq.com>
2024-05-08 01:52:38 +0800

    新增测试平台

---
+++ example/MX_G431_CT117E/MDK-ARM/RTE/_MX_G431_CT117E/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'MX_G431_CT117E' 
+ * Target:  'MX_G431_CT117E' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32g4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
